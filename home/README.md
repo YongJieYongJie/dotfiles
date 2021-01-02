7e914d34bcdd4256a5d65e41950bfa4e31faf2ab
YongJieYongJie <KhooYongJie@gmx.com>
2021-01-02 18:13:00 +0800

    Move dotfiles located in ~ to "home" folder

---
+++ home/README.md
@@ -0,0 +1,4 @@
+# HOME
+
+Files inside this folder are generally located directly in the home directory
+(i.e., `~`).
--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env sh
 
-curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
-curl -L git.io/antigen > ~/.antigen.zsh
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
 scriptDir=`dirname "$0"`
 absScriptDir=`cd $scriptDir;pwd`
 homeDir=~
 
 echo "[*] Executing installation script located at `dirname "$0"`..."
 
-for dotfile in .bash_profile .bashrc .gitconfig .inputrc .profile .tmux.conf \
-	.vimrc .zshrc
+for dotfile in home/.gitconfig home/.profile home/.tmux.conf home/.vimrc \
+    home/.zshrc
 do
-	echo "[*] Creating a symlink at $homeDir/$dotfile pointing to $absScriptDir/$dotfile..."
-	if test -f "$homeDir/$dotfile" || test -L "$homeDir/$dotfile"; then
-		backupFile=$dotfile.bak.`date +%Y%^b%d_%H%M`
-		echo "[!] $dotfile already exist at $homeDir, backing up to $backupFile"
-		mv "$homeDir/$dotfile" "$homeDir/$backupFile"
-	fi
-	ln -s "$absScriptDir/$dotfile" "$homeDir/$dotfile"
+    echo "[*] Creating a symlink at $homeDir/$dotfile pointing to $absScriptDir/$dotfile..."
+    if test -f "$homeDir/$dotfile" || test -L "$homeDir/$dotfile"; then
+        backupFile=$dotfile.bak.`date +%Y%^b%d_%H%M`
+        echo "[!] $dotfile already exist at $homeDir, backing up to $backupFile"
+        mv "$homeDir/$dotfile" "$homeDir/$backupFile"
+    fi
+    ln -s "$absScriptDir/$dotfile" "$homeDir/$dotfile"
 done
 
+# Install Git prompt for Git-related information in prompt shell.
+curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 
+# Install plugin manager for Vim.
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install plugins for Vim.
 vim -c 'PluginInstall' -c 'qa!'
 
 # Install package manager for zsh: Zinit.
