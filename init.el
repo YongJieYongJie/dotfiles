a545a0cafe214b12b8f2bc4898baf2a568c51dd5
YongJieYongJie <KhooYongJie@gmx.com>
2021-05-16 12:18:08 +0800

    Add to init.el Ivy-mode and related functionalities
     - Ivy-mode is a completion frontend that display options in a vertical
       list (among other things)
     - Counsel provides Ivy-based alternative to many default Emacs's
       commands
     - Swiper provides Ivy-based alternative to isearch that shows preview
       of all matches in the file

--- init.el
+++ init.el
@@ -298,6 +298,63 @@
   (define-key company-active-map (kbd "C-n") #'company-select-next)
   (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
+;;; Counsel (includes the dependencies Ivy and Swiper)
+;;; (https://oremacs.com/swiper/)
+;;; Ivy is a generic completion frontend
+;;; Swiper is an alternative isearch with a preview of all matching lines
+;;; Counsel is a collection of Ivy-enhanced versions of common Emacs commands
+(use-package counsel
+  :ensure t
+  :delight ivy-mode
+  :config
+  (ivy-mode 1)
+
+  ;; Basic customization as recommended on official documentation
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format " (%d/%d) ")
+
+  ;; Below are the key bindings as listed on the official documentation.
+  ;; TODO: Uncomment them out as I explore new functionalities.
+
+  ;; Ivy-based interface to standard commands
+  :bind (;;("C-s" . swiper-isearch)
+         ("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("M-y" . counsel-yank-pop)
+         ;;("<f1> f" . counsel-describe-function)
+         ;;("<f1> v" . counsel-describe-variable)
+         ;;("<f1> l" . counsel-find-library)
+         ;;("<f2> i" . counsel-info-lookup-symbol)
+         ;;("<f2> u" . counsel-unicode-char)
+         ;;("<f2> j" . counsel-set-variable)
+         ;;("C-x b" . ivy-switch-buffer)
+         ;;("C-c v" . ivy-push-view)
+         ;;("C-c V" . ivy-pop-view)
+
+         ;; Ivy-based interface to shell and system tools
+         ;;("C-c c" . counsel-compile)
+         ;;("C-c g" . counsel-git)
+         ;;("C-c j" . counsel-git-grep)
+         ;;("C-c L" . counsel-git-log)
+         ;;("C-c k" . counsel-rg)
+         ;;("C-c m" . counsel-linux-app)
+         ;;("C-c n" . counsel-fzf)
+         ;;("C-x l" . counsel-locate)
+         ;;("C-c J" . counsel-file-jump)
+         ;;("C-S-o" . counsel-rhythmbox)
+         ;;("C-c w" . counsel-wmctrl)
+
+         ;; Ivy-resume and other commands
+         ("C-c C-r" . ivy-resume)
+         ;;("C-c b" . counsel-bookmark)
+         ;;("C-c d" . counsel-descbinds)
+         ;;("C-c g" . counsel-git)
+         ;;("C-c o" . counsel-outline)
+         ;;("C-c t" . counsel-load-theme)
+         ;;("C-c F" . counsel-org-file)
+         ))
+
+
 ;;;-----------------------------------------------------------------------------
 ;;; Text Mode: General
 ;;;-----------------------------------------------------------------------------
