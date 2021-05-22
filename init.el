97a50f8b025712258169733a8d6a67b1b1dbe69b
YongJieYongJie <KhooYongJie@gmx.com>
2021-05-22 08:39:33 +0800

    Update init.el to use company-tng-mode
     - Hitting the <TAB> key will cycle through suggested completions, and
       update the buffer at the same time, so there is no need to press
       <ENTER> or <c-y> after selecting the desired completion entry.

--- init.el
+++ init.el
@@ -13,11 +13,6 @@
 ;;     should I have a section for company-mode, or should those
 ;;     settings be subsumed under the settings for the programming
 ;;     languages modes).
-;;   - Configure company-mode completion to rely only on <TAB>, see
-;;     company-tng.el
-;;     (https://github.com/company-mode/company-mode/blob/master/company-tng.el).
-;;     - Keys like <RET> should not trigger completion.
-;;     - Motion keys should work.
 ;;   - Standardize config files across devices.
 ;;   - Add proper comments.
 ;;   - Standardize use of # symbol.
@@ -291,59 +286,21 @@
 ;;; Major Packages
 ;;;-----------------------------------------------------------------------------
 
-;;; company-mode (http://company-mode.github.io/)
-;;; Modular in-buffer completion framework for Emacs.
-;; Yong Jie 2020.08.20: below doesn't seem to work.
-;; Prevent suggestions from being triggered automatically. In particular,
-;; this makes it so that:
-;; - TAB will always complete the current selection.
-;; - RET will only complete the current selection if the user has explicitly
-;;   interacted with Company.
-;; - SPC will never complete the current selection.
-;;
-;; Based on:
-;; - https://github.com/company-mode/company-mode/issues/530#issuecomment-226566961
-;; - https://emacs.stackexchange.com/a/13290/12534
-;; - http://stackoverflow.com/a/22863701/3538165
-;;
-;; See also:
-;; - https://emacs.stackexchange.com/a/24800/12534
-;; - https://emacs.stackexchange.com/q/27459/12534
-(with-eval-after-load 'company
-  ;; <return> is for windowed Emacs; RET is for terminal Emacs
-  (dolist (key '("<return>" "RET"))
-    ;; Here we are using an advanced feature of define-key that lets
-    ;; us pass an "extended menu item" instead of an interactive
-    ;; function. Doing this allows RET to regain its usual
-    ;; functionality when the user has not explicitly interacted with
-    ;; Company.
-    (define-key company-active-map (kbd key)
-      `(menu-item nil company-complete
-                  :filter ,(lambda (cmd)
-                             (when (company-explicit-action-p)
-                               cmd)))))
-  (define-key company-active-map (kbd "TAB") #'company-complete-selection)
-  (define-key company-active-map (kbd "SPC") nil)
-  (setq company-show-number t)
-  ;; Company appears to override the above keymap based on company-auto-complete-chars.
-  ;; Turning it off ensures we have full control.
-  (setq company-auto-complete-chars nil))
 
 (use-package company
   :ensure t
   :config
+
+  ;; Immediately show completions after two characters are typed
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 2)
-;;  (company-tng-configure-default)
-;;  (setq company-frontends
-;;        '(company-tng-frontend))
+
+  ;; Enable company-tng-mode, which uses tab key for navigating through
+  ;; suggestions, and inserting into the buffer the currently suggestion as we
+  ;; are tabbing through. For more, details, see the comany-tng.el file.
+  (add-hook 'after-init-hook 'company-tng-mode)
   )
 
-(with-eval-after-load 'company
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
-  (define-key company-active-map (kbd "C-n") #'company-select-next)
-  (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
 ;;; Counsel (includes the dependencies Ivy and Swiper)
 ;;; (https://oremacs.com/swiper/)
