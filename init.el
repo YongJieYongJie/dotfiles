2ec694c6b61a1fc3d813641427405bb92ffb28d7
YongJieYongJie <KhooYongJie@gmx.com>
2021-07-04 16:03:16 +0800

    Update init.el: add basic C-related LSP config

--- init.el
+++ init.el
@@ -1361,6 +1361,18 @@ When repeatedly called we cycle through three states:
 
 
 ;;;-----------------------------------------------------------------------------
+;;; Programming Mode: C
+;;;-----------------------------------------------------------------------------
+
+
+;; Copied from https://github.com/MaskRay/ccls/wiki/lsp-mode#use-package
+(use-package ccls
+  :ensure t
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
+
+;;;-----------------------------------------------------------------------------
 ;;; Tools: Docker
 ;;;-----------------------------------------------------------------------------
 
