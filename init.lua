dcae1074c2eef2d1e717041a9449bc031e4c2618
YongJieYongJie <KhooYongJie@gmx.com>
2022-03-07 08:07:13 +0800

    Update vim config: add nvim-treesitter config

--- init.lua
+++ init.lua
@@ -252,4 +252,33 @@ require'lspconfig'.sumneko_lua.setup {
 ------------------------------------------------------------------------ lua ---}}}
 
 ------------------------------------------------------------- nvim-lspconfig ---}}}
+------------------------------------------------------------ nvim-treesitter ---{{{
+
+-- From https://github.com/nvim-treesitter/nvim-treesitter/tree/cada76c4901e2389c0f82ac11d0c9c61d5205e90
+require'nvim-treesitter.configs'.setup {
+  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "maintained",
+
+  -- Install languages synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- List of parsers to ignore installing
+  -- ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    -- disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = true,
+  },
+}
+
+------------------------------------------------------------ nvim-treesitter ---}}}
 -- vim:fdm=marker
